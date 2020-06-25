e9bbb9fd06a20b6bd1925d43b0b0a76ad840a183
urushibata-a <urushibata.a@isoroot.jp>
2020-06-26 01:06:36 +0900

    2 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, TodoListComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
